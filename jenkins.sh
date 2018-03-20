51a71974c9ae7762e67dabd4a9ea820f4d6507f6
anderson <15840959355@126.com>
2018-03-20 16:33:13 +0800

    change4

--- jenkins.sh
+++ jenkins.sh
@@ -3,25 +3,25 @@ APP_NAME="Jenkins"
 # 证书
 # CODE_SIGN_DEVELOPMENT="iPhone Distribution: SunEee Weilian Technology Development Co., Ltd."
 # info.plist路径
-project_infoplist_path="./${APP_NAME}/Info.plist"
+# project_infoplist_path="./${APP_NAME}/Info.plist"
 
-echo ${project_infoplist_path}
+# echo ${project_infoplist_path}
 
 #取版本号
 #bundleShortVersion=$(/usr/libexec/PlistBuddy -c "-----> print CFBundleShortVersionString" "${project_infoplist_path}")
-bundleShortVersion = 1.0
+# bundleShortVersion = 1.0
 #取build值
 #bundleVersion=$(/usr/libexec/PlistBuddy -c "print CFBundleVersion" "${project_infoplist_path}")
-bundleVersion = 2.0
+# bundleVersion = 2.0
 echo "anderson log ==========> "
 
 DATE="$(date +%Y%m%d)"
-IPANAME="${APP_NAME}_V${bundleShortVersion}_${DATE}.ipa"
+IPANAME="${APP_NAME}_${DATE}.ipa"
 
 #要上传的ipa文件路径
-IPA_PATH="$HOME/${IPANAME}"
-echo ${IPA_PATH}
-echo "${IPA_PATH}">> text.txt
+# IPA_PATH="$HOME/${IPANAME}"
+# echo ${IPA_PATH}
+# echo "${IPA_PATH}">> text.txt
 
 echo "=================clean================="
 xcodebuild -workspace "${APP_NAME}.xcworkspace" -scheme "${APP_NAME}"  -configuration 'Debug' clean
