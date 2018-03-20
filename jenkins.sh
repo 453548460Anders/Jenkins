29ddba92a75a873794195cf0bb6a463e5346ebc5
anderson <15840959355@126.com>
2018-03-20 16:08:51 +0800

    change2

--- jenkins.sh
+++ jenkins.sh
@@ -1,16 +1,16 @@
 # 工程名
 APP_NAME="Jenkins"
 # 证书
-# CODE_SIGN_DISTRIBUTION="iPhone Distribution: SunEee Weilian Technology Development Co., Ltd."
+CODE_SIGN_DEVELOPMENT="iPhone Distribution: SunEee Weilian Technology Development Co., Ltd."
 # info.plist路径
 project_infoplist_path="./${APP_NAME}/Info.plist"
 
 #取版本号
-bundleShortVersion=$(/usr/libexec/PlistBuddy -c "-----> print CFBundleShortVersionString" "${project_infoplist_path}")
-
+#bundleShortVersion=$(/usr/libexec/PlistBuddy -c "-----> print CFBundleShortVersionString" "${project_infoplist_path}")
+bundleShortVersion = 1.0
 #取build值
-bundleVersion=$(/usr/libexec/PlistBuddy -c "print CFBundleVersion" "${project_infoplist_path}")
-
+#bundleVersion=$(/usr/libexec/PlistBuddy -c "print CFBundleVersion" "${project_infoplist_path}")
+bundleVersion = 2.0
 echo "anderson log ==========> "
 
 DATE="$(date +%Y%m%d)"
@@ -25,4 +25,7 @@ echo "=================clean================="
 xcodebuild -workspace "${APP_NAME}.xcworkspace" -scheme "${APP_NAME}"  -configuration 'Debug' clean
 
 echo "+++++++++++++++++build+++++++++++++++++"
-xcodebuild -workspace "${APP_NAME}.xcworkspace" -scheme "${APP_NAME}" -sdk iphoneos -configuration 'Debug' CODE_SIGN_IDENTITY="${CODE_SIGN_DEVELOPMENT}" SYMROOT='$(PWD)'
+xcodebuild -workspace "${APP_NAME}.xcworkspace" -scheme "${APP_NAME}" -sdk iphoneos -configuration 'Debug'
+
+#CODE_SIGN_IDENTITY="${CODE_SIGN_DEVELOPMENT}" SYMROOT='$(PWD)'
+# xcodebuild -workspace "${APP_NAME}.xcworkspace" -scheme "${APP_NAME}" -sdk iphoneos -configuration 'Debug' CODE_SIGN_IDENTITY="${CODE_SIGN_DEVELOPMENT}" SYMROOT='$(PWD)'
